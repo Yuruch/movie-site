8ae37e508cd526390285c6041d9c20ac1aa712ae
Yuruch <fuceqwertyt@gmail.com>
2024-07-17 13:50:22 +0300

    add models and AUTH_USER_MODEL to settings.py

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,59 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import SET_NULL, CASCADE
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField(
+        validators=(
+            MinValueValidator(6),
+            MaxValueValidator(100)
+        )
+    )
+    movies = models.ManyToManyField("Movie", related_name="directors")
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField(
+        validators=(
+            MinValueValidator(6),
+            MaxValueValidator(100)
+        )
+    )
+    movies = models.ManyToManyField("Movie", related_name="actors")
+
+
+class Review(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    rating = models.IntegerField(
+        validators=(
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        )
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Movie(models.Model):
+    director = models.ManyToManyField(Director, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    rating = models.DecimalField(max_digits=4, decimal_places=2)
+    reviews = models.ForeignKey(Review, on_delete=CASCADE)
+    # poster = models.ImageField()
+
+
+class User(AbstractUser):
+    # profile_pic = models.ImageField()
+    favourite_films = models.ManyToManyField(Movie, related_name="users")
