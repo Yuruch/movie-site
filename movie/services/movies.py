f24f87b31980fc2e370c93fa7fa3438105f81f3c
Yuruch <fuceqwertyt@gmail.com>
2024-07-21 16:31:30 +0300

    fix rounding

--- movie/services/movies.py
+++ movie/services/movies.py
@@ -1,11 +1,12 @@
 from django.db.models import QuerySet, Avg
+from django.db.models.functions import Round
 
 from movie.models import Movie, User, Genre
 
 
 def best_movies(number: int) -> QuerySet:
     return Movie.objects.annotate(
-        avg_rating=Avg("reviews__rating")
+        avg_rating=Round(Avg("reviews__rating"), 2)
     ).order_by("-avg_rating")[:number]
 
 
@@ -14,7 +15,7 @@ def movie_you_like(user: User, number: int) -> QuerySet:
     return Movie.objects.filter(
         genres__in=genres
     ).annotate(
-        avg_rating=Avg("reviews__rating")
+        avg_rating=Round(Avg("reviews__rating"), 2)
     ).filter(
         avg_rating__gte=5
     ).order_by("-avg_rating")[:number]
