673f98e4b5990aad2062a43c44c774263812d98f
Yuruch <fuceqwertyt@gmail.com>
2024-07-23 09:47:06 +0300

    add render to allowed_hosts

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "movie-site-0otf.onrender.com"]
 
 
 # Application definition
