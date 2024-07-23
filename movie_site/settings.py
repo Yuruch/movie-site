7e18147498c2c7d15be4544d3dfa4f4c1553806b
Yuruch <fuceqwertyt@gmail.com>
2024-07-23 14:46:02 +0300

    add space

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-ALLOWED_HOSTS = ["127.0.0.1","localhost", "movie-site-0otf.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "movie-site-0otf.onrender.com"]
 
 
 # Application definition
