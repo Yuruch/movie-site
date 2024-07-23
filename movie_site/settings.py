7f5600e3e3fb794ebdc95fd7b22bc7876645c5b6
Yuruch <fuceqwertyt@gmail.com>
2024-07-23 09:14:04 +0300

    remove django secret key for deployment

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","django-insecure-a4+tvj=f=074nytk&qh^l1x*+z^v%l1vrh!mr=t#^)w)^n3xly")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
