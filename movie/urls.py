cf3b3bdb51e769ae401295f10ca3fdb6df70ce1a
Yuruch <fuceqwertyt@gmail.com>
2024-07-23 17:57:05 +0300

    fix code style in urls.py

--- movie/urls.py
+++ movie/urls.py
@@ -26,24 +26,56 @@ from movie.views import (
 )
 
 urlpatterns = [
-    path("", index, name="home"),
-    path("user/<int:pk>/", user_detail_view, name="user-detail"),
-    path("user/<int:pk>/update", UserUpdateView.as_view(), name="user-update"),
-    path("actors/", ActorListView.as_view(), name="actor-list"),
-    path("actors/create/", ActorCreateView.as_view(), name="actor-create"),
+    path(
+        "",
+        index,
+        name="home"
+    ),
+    path(
+        "user/<int:pk>/",
+        user_detail_view,
+        name="user-detail"
+    ),
+    path(
+        "user/<int:pk>/update",
+        UserUpdateView.as_view(),
+        name="user-update"
+    ),
+    path(
+        "actors/",
+        ActorListView.as_view(),
+        name="actor-list"
+    ),
+    path(
+        "actors/create/",
+        ActorCreateView.as_view(),
+        name="actor-create"
+    ),
     path(
         "actors/<int:pk>/update/",
         ActorUpdateView.as_view(),
         name="actor-update"
     ),
-    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
+    path(
+        "actors/<int:pk>/",
+        ActorDetailView.as_view(),
+        name="actor-detail"
+    ),
     path(
         "actors/<int:pk>/delete/",
         ActorDeleteView.as_view(),
         name="actor-delete"
     ),
-    path("movies/", MovieListView.as_view(), name="movie-list"),
-    path("movies/create/", MovieCreateView.as_view(), name="movie-create"),
+    path(
+        "movies/",
+        MovieListView.as_view(),
+        name="movie-list"
+    ),
+    path(
+        "movies/create/",
+        MovieCreateView.as_view(),
+        name="movie-create"
+    ),
     path(
         "movies/<int:pk>/add_favourite/",
         toggle_add_to_favourites,
@@ -59,7 +91,11 @@ urlpatterns = [
         MovieDeleteView.as_view(),
         name="movie-delete"
     ),
-    path("movie/<int:pk>/add_review/", add_review, name="review-create"),
+    path(
+        "movie/<int:pk>/add_review/",
+        add_review,
+        name="review-create"
+    ),
     path(
         "movie/<int:movie_pk>/review/<int:review_pk>/update/",
         update_review,
@@ -70,8 +106,16 @@ urlpatterns = [
         delete_review,
         name="review-delete"
     ),
-    path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
-    path("directors/", DirectorListView.as_view(), name="director-list"),
+    path(
+        "movies/<int:pk>/",
+        MovieDetailView.as_view(),
+        name="movie-detail"
+    ),
+    path(
+        "directors/",
+        DirectorListView.as_view(),
+        name="director-list"
+    ),
     path(
         "directors/create/",
         DirectorCreateView.as_view(),
